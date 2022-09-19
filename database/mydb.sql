cdff8b95e3e65e18576a61354c396fa60730754c
LeoMontoy <gmontoyrodriguez@gmail.com>
2022-09-19 19:13:04 -0300

    db-conProdDeEjemplo

--- database/mydb.sql
+++ database/mydb.sql
@@ -1,116 +1,231 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Versión del servidor:         10.4.20-MariaDB - mariadb.org binary distribution
--- SO del servidor:              Win64
--- HeidiSQL Versión:             11.3.0.6295
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 19-09-2022 a las 23:48:03
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de datos: `mydb`
+--
 
--- Volcando estructura de base de datos para mydb
-CREATE DATABASE IF NOT EXISTS `mydb` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `mydb`;
+-- --------------------------------------------------------
 
--- Volcando estructura para tabla mydb.brand
-CREATE TABLE IF NOT EXISTS `brand` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+--
+-- Estructura de tabla para la tabla `brand`
+--
 
--- Volcando datos para la tabla mydb.brand: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `brand` DISABLE KEYS */;
+CREATE TABLE `brand` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `brand`
+--
+
 INSERT INTO `brand` (`id`, `name`) VALUES
-	(1, 'Jordan'),
-	(2, 'Nike'),
-	(3, 'Adidas'),
-	(4, 'Under Armour'),
-	(5, 'Reebok'),
-	(6, 'Converse');
-/*!40000 ALTER TABLE `brand` ENABLE KEYS */;
+(1, 'Jordan'),
+(2, 'Nike'),
+(3, 'Adidas'),
+(4, 'Under Armour'),
+(5, 'Reebok'),
+(6, 'Converse');
 
--- Volcando estructura para tabla mydb.color
-CREATE TABLE IF NOT EXISTS `color` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+-- --------------------------------------------------------
 
--- Volcando datos para la tabla mydb.color: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `color` DISABLE KEYS */;
+--
+-- Estructura de tabla para la tabla `color`
+--
+
+CREATE TABLE `color` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `color`
+--
+
 INSERT INTO `color` (`id`, `name`) VALUES
-	(1, 'White'),
-	(2, 'Black'),
-	(3, 'Red'),
-	(4, 'Blue'),
-	(5, 'Grey'),
-	(6, 'Yellow');
-/*!40000 ALTER TABLE `color` ENABLE KEYS */;
+(1, 'White'),
+(2, 'Black'),
+(3, 'Red'),
+(4, 'Blue'),
+(5, 'Grey'),
+(6, 'Yellow');
 
--- Volcando estructura para tabla mydb.images
-CREATE TABLE IF NOT EXISTS `images` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `images`
+--
+
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL,
   `url` varchar(500) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_images_products` (`product_id`),
-  CONSTRAINT `FK_images_products` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `product_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla mydb.images: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `images` DISABLE KEYS */;
+--
+-- Volcado de datos para la tabla `images`
+--
+
 INSERT INTO `images` (`id`, `url`, `product_id`) VALUES
-	(1, 'https://sneakerbardetroit.com/wp-content/uploads/2022/02/Air-Jordan-1-Brotherhood-555088-706-Release-Date-1.jpeg', 1);
-/*!40000 ALTER TABLE `images` ENABLE KEYS */;
+(1, 'https://sneakerbardetroit.com/wp-content/uploads/2022/02/Air-Jordan-1-Brotherhood-555088-706-Release-Date-1.jpeg', 1),
+(5, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTymfthShxfJF2xqCGJQ7PxcrLS_D7_YsqcZw&usqp=CAU', 4),
+(15, 'https://i.ebayimg.com/images/g/CxEAAOSw~atb2wbV/s-l500.jpg', 111);
 
--- Volcando estructura para tabla mydb.material
-CREATE TABLE IF NOT EXISTS `material` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+-- --------------------------------------------------------
 
--- Volcando datos para la tabla mydb.material: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `material` DISABLE KEYS */;
+--
+-- Estructura de tabla para la tabla `material`
+--
+
+CREATE TABLE `material` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `material`
+--
+
 INSERT INTO `material` (`id`, `name`) VALUES
-	(1, 'Leather'),
-	(2, 'Synthetic'),
-	(3, 'Textile'),
-	(4, 'Canvas'),
-	(5, 'Rubber');
-/*!40000 ALTER TABLE `material` ENABLE KEYS */;
+(1, 'Leather'),
+(2, 'Synthetic'),
+(3, 'Textile'),
+(4, 'Canvas'),
+(5, 'Rubber');
 
--- Volcando estructura para tabla mydb.products
-CREATE TABLE IF NOT EXISTS `products` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `price` int(11) NOT NULL,
   `description` varchar(500) NOT NULL,
   `brand_id` int(11) NOT NULL,
   `material_id` int(11) NOT NULL,
-  `color_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_products_brand` (`brand_id`),
-  KEY `FK_products_material` (`material_id`),
-  KEY `FK_products_color` (`color_id`),
-  CONSTRAINT `FK_products_brand` FOREIGN KEY (`brand_id`) REFERENCES `brand` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_products_color` FOREIGN KEY (`color_id`) REFERENCES `color` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_products_material` FOREIGN KEY (`material_id`) REFERENCES `material` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `color_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla mydb.products: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+--
+-- Volcado de datos para la tabla `products`
+--
+
 INSERT INTO `products` (`id`, `name`, `price`, `description`, `brand_id`, `material_id`, `color_id`) VALUES
-	(1, 'Jordan 1 High OG “Brotherhood”', 170, 'Dressed in a University Gold, Light Bordeaux, and White color scheme. This Air Jordan 1 features White leather side panels, nylon tongues, and midsole paired with Gold and Purple overlays nodding to the colors of the fraternity. A White midsole atop a Yellow rubber outsole completes the design.', 1, 1, 1);
-/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+(1, 'Jordan 1 High OG “Brotherhood”', 170, 'Dressed in a University Gold, Light Bordeaux, and White color scheme. This Air Jordan 1 features White leather side panels, nylon tongues, and midsole paired with Gold and Purple overlays nodding to the colors of the fraternity. A White midsole atop a Yellow rubber outsole completes the design.', 1, 1, 1),
+(4, 'Converse Chuck Taylor All Star Hi', 100, 'Estos zapatos clásicos tienen una parte superior de lona negra y una puntera y suela de goma en color blanco. Son terminados con gráfico logo Converse imprimido por el lado', 6, 3, 2),
+(111, 'Under Armour Men Black Canvas', 40000, 'Full style, Recomendado para el hombre elegante, de buen vestir.', 4, 4, 2);
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `brand`
+--
+ALTER TABLE `brand`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `color`
+--
+ALTER TABLE `color`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_images_products` (`product_id`);
+
+--
+-- Indices de la tabla `material`
+--
+ALTER TABLE `material`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_products_brand` (`brand_id`),
+  ADD KEY `FK_products_material` (`material_id`),
+  ADD KEY `FK_products_color` (`color_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `brand`
+--
+ALTER TABLE `brand`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `color`
+--
+ALTER TABLE `color`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `images`
+--
+ALTER TABLE `images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT de la tabla `material`
+--
+ALTER TABLE `material`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=112;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `images`
+--
+ALTER TABLE `images`
+  ADD CONSTRAINT `FK_images_products` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `FK_products_brand` FOREIGN KEY (`brand_id`) REFERENCES `brand` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_products_color` FOREIGN KEY (`color_id`) REFERENCES `color` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_products_material` FOREIGN KEY (`material_id`) REFERENCES `material` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
